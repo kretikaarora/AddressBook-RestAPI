babb690ba9cabb288c1f50f1fda2a4bde7c5f266
kretika arora <kretikaarora@gmail.com>
2020-11-12 22:50:09 +0530

    [Kretika Arora]Getting all Employee Details using Get Method

---
+++ AddressBook_RestAPI/EmployeeDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook_RestAPI
+{
+    public class EmployeeDetails
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zip { get; set; }
+        public double phoneNumber { get; set; }
+        public string email { get; set; }
+        public int id { get; set; }
+        public int completeAddressId { get; set; }
+        public string addressBookName { get; set; }
+        public int typeId { get; set; }
+        public string typeName { get; set; }
+        
+    }
+}
---
+++ AddressBook_RestAPI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBook_RestAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To AddressBook RestAPI");
+        }
+    }
+}
